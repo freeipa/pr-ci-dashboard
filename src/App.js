7c6908ffb1614ba3e69994496ff7b4cb319cc8cd
Petr Vobornik <pvoborni@gmail.com>
2019-02-04 20:58:00 +0100

    Add react router with basic routes
    Signed-off-by: Petr Vobornik <pvoborni@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,78 +1,87 @@
 import React, { Component } from 'react';
-import Content from './Content';
 import './App.css';
+import { Route, Redirect, withRouter, Switch } from "react-router-dom";
 
 import {
   VerticalNav,
   VerticalNavItem,
   VerticalNavMasthead,
-  VerticalNavBrand
+  VerticalNavBrand,
+  Grid
 } from 'patternfly-react';
 
 import pfLogo from 'patternfly/dist/img/logo-alt.svg';
 import pfBrand from 'patternfly/dist/img/brand-alt.svg';
-/* import { patternfly } from 'patternfly-react/dist/js/common/patternfly'; */
+
+const Content = (props) => {
+    return <Grid fluid className="container-pf-nav-pf-vertical">
+        <Grid.Row>
+            <Grid.Col xs={12}>
+                {props.children}
+            </Grid.Col>
+        </Grid.Row>
+    </Grid>
+}
+
+const PRs = () => <Content><h2>Pull Requests</h2></Content>;
+const Nighlies = () => <Content><h2>Nighlies</h2></Content>;
+const Jobs = () => <Content><h2>Jobs</h2></Content>;
 
 class App extends Component {
   constructor() {
     super();
     this.menu = [{
-      to: "key",
-      title: "My menu Item",
-      iconClass: "key",
-      active: true
+      to: "/prs",
+      title: "Pull Requests",
+      iconClass: "fa fa-code-fork"
     },
     {
-      to: "key2",
-      title: "Item 2",
-      iconClass: "success",
-      active: true
+      to: "/nightlies",
+      title: "Nighly runs",
+      iconClass: "fa fa-moon-o"
+    },
+    {
+      to: "/jobs",
+      title: "Jobs",
+      iconClass: "fa fa-wrench"
     }]
   }
 
   render() {
 
+    const history = this.props.history;
     const vertNavItems = this.menu.map(item => (
       <VerticalNavItem
         key={item.to}
         title={item.title}
         iconClass={item.iconClass}
-        active={false}
-        onClick={() => this.navigateTo(item.to)}
+        active={history.location.pathname === item.to}
+        onClick={() => {
+            history.push(item.to);
+        }}
       >
       </VerticalNavItem>
     ));
 
-
     return (
-      /*       <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
-              </header>
-              <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-              </p>
-            </div> */
-      <React.Fragment>
-        <VerticalNav persistentSecondary={false}>
-          <VerticalNavMasthead>
-            <VerticalNavBrand titleImg={pfBrand} iconImg={pfLogo} />
-          </VerticalNavMasthead>
-          {vertNavItems}
-          {/* <VerticalNavItem
-          key="abc"
-          title="Ipsum"
-          iconClass="fa fa-dashboard"
-          active
-          onClick={() => this.navigateTo('/')}
-        /> */}
-        </VerticalNav>
-        <Content />
-        {/*  {this.renderContent()} */}
-      </React.Fragment>
+        <div>
+            <VerticalNav persistentSecondary={false}>
+                <VerticalNavMasthead>
+                    <VerticalNavBrand titleImg={pfBrand} iconImg={pfLogo} />
+                </VerticalNavMasthead>
+                {vertNavItems}
+            </VerticalNav>
+
+            <Switch>
+                <Route path="/prs" component={PRs} />
+                <Route path="/nightlies" component={Nighlies} />
+                <Route path="/jobs" component={Jobs} />
+                <Redirect exact from="/" to="/prs" />
+            </Switch>
+
+        </div>
     );
   }
 }
-
+App = withRouter(App);
 export default App;
