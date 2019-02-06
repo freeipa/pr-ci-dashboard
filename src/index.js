13ad1b8dda196618bf7d666337191d5ded8f4817
Petr Vobornik <pvoborni@redhat.com>
2019-02-06 10:16:12 +0100

    Set base url to 'pcd'
    So that it can be run on server without any config from 'pcd'
    directory. I will be most likely removed/replaced later.
    
    Signed-off-by: Petr Vobornik <pvoborni@redhat.com>

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+    <Router basename='pcd'>
+        <App />
+    </Router>, document.getElementById('root'));
 registerServiceWorker();
