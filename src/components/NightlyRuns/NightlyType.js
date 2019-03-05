1f68bba4f081c63fd3f3866fef5eb2cdfcbdbe2f
Petr Vobornik <pvoborni@gmail.com>
2019-03-05 23:30:16 +0100

    Fix all lint issues in NightlyType.js
    Removing tech dept.
    
    Signed-off-by: Petr Vobornik <pvoborni@gmail.com>

--- src/components/NightlyRuns/NightlyType.js
+++ src/components/NightlyRuns/NightlyType.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { LoadingState} from 'patternfly-react';
+import PropTypes from 'prop-types';
+import { LoadingState } from 'patternfly-react';
 import { getNightlyType } from './NighlyService';
 import NightlyTypeRow from './NightlyTypeRow';
 import NightlyTypeHeader from './NightlyTypeHeader';
@@ -7,60 +8,67 @@ import Content from '../../Content';
 
 
 class NightlyType extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
             nightlyType: null,
-            loading: false
+            loading: false,
         };
     }
 
     componentWillMount() {
+        const { match } = this.props;
         this.setState({
-            ...this.state,
-            loading: true
+            loading: true,
         });
-        getNightlyType(this.props.match.params.nightlyTypeName).then(nightlyType => {
+        getNightlyType(match.params.nightlyTypeName).then((nightlyType) => {
             this.setState({
-                nightlyType: nightlyType,
-                loading: false
+                nightlyType,
+                loading: false,
             });
         });
     }
 
-    renderNightly(nightlyType) {
-        return <React.Fragment>
-            <h2>{nightlyType.name}</h2>
-            <table className='table table-striped table-bordered'>
-            <thead>
-                <tr>
-                    <NightlyTypeHeader nightlies={nightlyType.nightlies} />
-                </tr>
-            </thead>
-                <tbody>
-                    { nightlyType.jobs.map((job, i) =>
-                        <NightlyTypeRow job={job} key={i}/>) }
-                </tbody>
-            </table>
-        </React.Fragment>
+    static renderNightly(nightlyType) {
+        return (
+            <React.Fragment>
+                <h2>{nightlyType.name}</h2>
+                <table className="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <NightlyTypeHeader nightlies={nightlyType.nightlies} />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { nightlyType.jobs.map(job => (
+                            <NightlyTypeRow job={job} key={job.name} />
+                        ))}
+                    </tbody>
+                </table>
+            </React.Fragment>
+        );
     }
 
     render() {
-        const nightlyType = this.state.nightlyType;
-        let out = "";
-        if (!this.state.loading && nightlyType) {
-            out = this.renderNightly(nightlyType);
+        const { nightlyType, loading } = this.state;
+        let out = '';
+        if (!loading && nightlyType) {
+            out = NightlyType.renderNightly(nightlyType);
         }
 
         return (
             <Content>
-            <LoadingState loading={this.state.loading} size="lg" loadingText=''>
-                {out}
-            </LoadingState>
+                <LoadingState loading={loading} size="lg" loadingText="">
+                    {out}
+                </LoadingState>
             </Content>
         );
     }
 }
+
+NightlyType.propTypes = {
+    // eslint-disable-next-line react/forbid-prop-types
+    match: PropTypes.object.isRequired, // Router match
+};
 
 export default NightlyType;
