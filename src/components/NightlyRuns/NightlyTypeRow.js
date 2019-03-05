a628b81bb8370d55d24cd422a7b43dc5af8d8a43
Petr Vobornik <pvoborni@gmail.com>
2019-03-06 00:52:40 +0100

    Links and Info in Nightly runs history view
    History view of Nighlty runs now contains links to
    * Artifacts directory
    * runner.log
    * results.html
    
    This can be used for quick navigation and investigation of issues.
    
    Signed-off-by: Petr Vobornik <pvoborni@gmail.com>

--- src/components/NightlyRuns/NightlyTypeRow.js
+++ src/components/NightlyRuns/NightlyTypeRow.js
@@ -1,18 +1,89 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function renderCell(result, i) {
-    if (!result) {
-        return <td key={i} className="jobResult empty" />;
-    }
+
+function resultArtifacts(targetUrl) {
+    if (!targetUrl) return '';
+
+    return (
+        <div className="artifacts">
+            <a href={targetUrl}>
+                <i className="fa fa-trophy" aria-hidden="true" title="Artifacts" />
+            </a>
+        </div>
+    );
+}
+
+function resultRunnerLog(targetUrl) {
+    if (!targetUrl) return '';
+
+    return (
+        <div className="runnerLog">
+            <a href={`${targetUrl}/runner.log.gz`}>
+                <i className="fa fa-file-text-o" aria-hidden="true" title="Runner Log" />
+            </a>
+        </div>
+    );
+}
+
+function resultResults(targetUrl, state) {
+    if (!targetUrl || state === 'ERROR') return '';
+
+    return (
+        <div className="runnerLog">
+            <a href={`${targetUrl}/report.html`}>
+                <i className="fa fa-pie-chart" aria-hidden="true" title="Results" />
+            </a>
+        </div>
+    );
+}
+
+function description(value) {
+    if (value.indexOf('no description') > -1) return '';
+    if (value.indexOf('\\(^_^)/') > -1) return '';
+    if (value.indexOf('The Travis CI build passed') > -1) return '';
+    if (!value) return '';
+    return <div className="description">{value}</div>;
+}
+
+function ResultInfo(props) {
+    const { result } = props;
+
+    return (
+        <div className="resultInfo">
+            {resultArtifacts(result.targetUrl)}
+            {resultRunnerLog(result.targetUrl)}
+            {resultResults(result.targetUrl, result.state)}
+            {description(result.description)}
+        </div>
+    );
+}
+
+ResultInfo.propTypes = {
+    // eslint-disable-next-line react/forbid-prop-types
+    result: PropTypes.object.isRequired,
+};
+
+function ResultCell(props) {
+    const { result } = props;
 
     return (
         <td
-            key={i}
             className={`jobResult ${result.state}`}
             title={result.state}
-        />
+        >
+            <ResultInfo result={result} />
+        </td>
     );
+}
+
+ResultCell.propTypes = {
+    // eslint-disable-next-line react/forbid-prop-types
+    result: PropTypes.object.isRequired,
+};
+
+function EmptyResult() {
+    return <td className="jobResult empty" />;
 }
 
 function NightlyTypeRow(props) {
@@ -21,7 +92,12 @@ function NightlyTypeRow(props) {
         <React.Fragment>
             <tr>
                 <td className="jobName">{job.name}</td>
-                { job.results.map((result, i) => renderCell(result, i)) }
+                { job.results.map((result, i) => {
+                    // for empty rows there is no value than index thus ignore:
+                    // eslint-disable-next-line react/no-array-index-key
+                    if (!result) return <EmptyResult key={i} />;
+                    return <ResultCell result={result} key={result.id} />;
+                })}
             </tr>
         </React.Fragment>
     );
