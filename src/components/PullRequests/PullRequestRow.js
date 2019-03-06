fd4736fd48c5cf1885ea31feac43ee61f201bb24
Petr Vobornik <pvoborni@gmail.com>
2019-03-06 22:42:45 +0100

    Fix lint issues in PullRequestRow
    Signed-off-by: Petr Vobornik <pvoborni@gmail.com>

--- src/components/PullRequests/PullRequestRow.js
+++ src/components/PullRequests/PullRequestRow.js
@@ -1,72 +1,88 @@
 import React from 'react';
-import PullRequestLabels from './PullRequestLabels'
-import ChecksSummary from './ChecksSummary'
-import ChecksList from './ChecksList'
+import PropTypes from 'prop-types';
+import { PullRequestLabels } from './PullRequestLabels';
+import { ChecksSummary } from './ChecksSummary';
+import { ChecksList } from './ChecksList';
 
 class PullRequestRow extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            expanded: false
-        }
+            expanded: false,
+        };
     }
 
-    handleExpand = () => {
-        this.setState({
-            ...this.state,
-            expanded: !this.state.expanded
-        });
+    handleExpand() {
+        this.setState(state => ({
+            expanded: !state.expanded,
+        }));
     }
 
     renderExanded() {
-        if (!this.state.expanded) return;
-        let pr = this.props.pr;
+        const { expanded } = this.state;
+        const { pr } = this.props;
+        if (!expanded) return '';
+        const { contexts } = pr.commits.nodes[0].commit.status;
         return (
-        <tr>
-            <td colSpan='3'>
-            </td>
-            <td colSpan='3'>
-                <ChecksList statuses={pr.commits.nodes[0].commit.status.contexts} />
-            </td>
-        </tr>
+            <tr>
+                <td colSpan="3" />
+                <td colSpan="3">
+                    <ChecksList statuses={contexts} />
+                </td>
+            </tr>
         );
     }
 
     render() {
-        let pr = this.props.pr;
-        let repo = this.props.repository;
+        const { pr, repository } = this.props;
+        const { contexts } = pr.commits.nodes[0].commit.status;
         return (
-        <React.Fragment>
-        <tr>
-            <td className='table-view-pf-select'
-                onClick={() => this.handleExpand()}>
-                <span className="fa fa-angle-right"></span>
-            </td>
-            <td className='pr-author'>
-                <img src={pr.author.avatarUrl} alt={pr.author.login} height="40px" />
-                <a href={`${repo.url}/pulls/${pr.author.login}`}
-                   title="Show PRs of author on GitHub">
-                    {pr.author.login}
-                </a>
-            </td>
-            <td className='pr-number'>
-                <a href={pr.url} title="Navigate to GitHub PR">#{pr.number}</a>
-            </td>
-            <td className='pr-title'>
-                <strong>{pr.title}</strong>
-            </td>
-            <td  className='pr-checks'>
-                <ChecksSummary statuses={pr.commits.nodes[0].commit.status.contexts} />
-            </td>
-            <td  className='pr-labels'>
-                <PullRequestLabels labels={pr.labels.nodes} />
-            </td>
-        </tr>
-        {this.renderExanded()}
-        </React.Fragment>
+            <React.Fragment>
+                <tr>
+                    <td className="table-view-pf-select">
+                        <button
+                            type="button"
+                            className="btn btn-link btn-lg"
+                            onClick={() => this.handleExpand()}
+                        >
+                            <i className="fa fa-angle-right" />
+                        </button>
+                    </td>
+                    <td className="pr-author">
+                        <img src={pr.author.avatarUrl} alt={pr.author.login} height="40px" />
+                        <a
+                            href={`${repository.url}/pulls/${pr.author.login}`}
+                            title="Show PRs of author on GitHub"
+                        >
+                            {pr.author.login}
+                        </a>
+                    </td>
+                    <td className="pr-number">
+                        <a href={pr.url} title="Navigate to GitHub PR">
+                            {`#${pr.number}`}
+                        </a>
+                    </td>
+                    <td className="pr-title">
+                        <strong>{pr.title}</strong>
+                    </td>
+                    <td className="pr-checks">
+                        <ChecksSummary statuses={contexts} />
+                    </td>
+                    <td className="pr-labels">
+                        <PullRequestLabels labels={pr.labels.nodes} />
+                    </td>
+                </tr>
+                {this.renderExanded()}
+            </React.Fragment>
         );
     }
 }
+
+PullRequestRow.propTypes = {
+    // eslint-disable-next-line react/forbid-prop-types
+    pr: PropTypes.object.isRequired,
+    // eslint-disable-next-line react/forbid-prop-types
+    repository: PropTypes.object.isRequired,
+};
 
 export default PullRequestRow;
