f5adb0f4a2d614fe3195b9a4c55c08d2d11f45dc
Petr Vobornik <pvoborni@redhat.com>
2019-11-26 15:23:21 +0100

    Fix lint issues in PullRequestsList.js
    Signed-off-by: Petr Vobornik <pvoborni@redhat.com>

--- src/components/PullRequests/PullRequestsList.js
+++ src/components/PullRequests/PullRequestsList.js
@@ -1,65 +1,70 @@
 import React from 'react';
-import PullRequestRow from './PullRequestRow'
-import { fetchPullRequests } from '../../prlist'
-import { LoadingState} from 'patternfly-react'
+import { LoadingState } from 'patternfly-react';
+import PullRequestRow from './PullRequestRow';
+import { fetchPullRequests } from '../../prlist';
 
 class PullRequestList extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
             repository: {},
-            loading: false
-        }
+            loading: false,
+        };
     }
 
     componentWillMount() {
-        this.setState({
-            ...this.state,
-            loading: true
-        })
-        fetchPullRequests({numOfPrs: 50, state: 'OPEN'}).then(repository => {
+        this.setState(state => ({
+            ...state,
+            loading: true,
+        }));
+
+        fetchPullRequests({ numOfPrs: 50, state: 'OPEN' }).then((repository) => {
             this.setState({
-                repository: repository,
-                loading: false
+                repository,
+                loading: false,
             });
-        })
+        });
     }
 
     getPrs() {
-        if (this.state.repository && this.state.repository.pullRequests) {
-            return this.state.repository.pullRequests.nodes;
-        } else {
-            return [];
+        const { repository } = this.state;
+        if (repository && repository.pullRequests) {
+            return repository.pullRequests.nodes;
         }
+        return [];
     }
 
     render() {
         const prs = this.getPrs();
-        const repository = this.state.repository;
+        const { repository, loading } = this.state;
 
         return (
-        <React.Fragment>
-            <h2>Pull Requests</h2>
-            <LoadingState loading={this.state.loading} size="lg" loadingText=''>
-            <table className='table table-striped table-bordered table-hover'>
-                <thead>
-                    <tr>
-                    <th></th>
-                    <th>Author</th>
-                    <th>Pull Request</th>
-                    <th>Title</th>
-                    <th>Checks</th>
-                    <th>Labels</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {prs.map((pr, i) => <PullRequestRow repository={repository}
-                                                    pr={pr} key={pr.number}/>)}
-                </tbody>
-            </table>
-            </LoadingState>
-        </React.Fragment>
+            <React.Fragment>
+                <h2>Pull Requests</h2>
+                <LoadingState loading={loading} size="lg" loadingText="">
+                    <table className="table table-striped table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th />
+                                <th>Author</th>
+                                <th>Pull Request</th>
+                                <th>Title</th>
+                                <th>Checks</th>
+                                <th>Labels</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {prs.map(pr => (
+                                <PullRequestRow
+                                    repository={repository}
+                                    pr={pr}
+                                    key={pr.number}
+                                />
+                            ))}
+                        </tbody>
+                    </table>
+                </LoadingState>
+            </React.Fragment>
         );
     }
 }
