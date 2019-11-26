ae13839c14fd0f56e86116c9c2876b6175e4308c
Petr Vobornik <pvoborni@redhat.com>
2019-11-26 15:33:11 +0100

    server: Redirect all GET request to index.html
    Currently in prod deployment navigation to anything but / failed.
    
    This should enable navigation to route and thus allow users to
    bookmark or sent links to various views.
    
    Signed-off-by: Petr Vobornik <pvoborni@redhat.com>

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.post('/api/world', (req, res) => {
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.get('/', (req, res) => {
+app.get('/.*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
